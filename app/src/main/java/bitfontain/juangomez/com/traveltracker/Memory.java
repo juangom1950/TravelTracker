7f03a7e98d4e61ad7918611de2428ffdb51fde44
juangom1950 <juangom73@gmail.com>
2015-08-11 17:58:52 -0400

    Finish S19L12

---
+++ app/src/main/java/bitfontain/juangomez/com/traveltracker/Memory.java
@@ -0,0 +1,16 @@
+package bitfontain.juangomez.com.traveltracker;
+
+/**
+ * Created by Juan on 8/11/2015.
+ */
+public class Memory {
+
+    double latitude;
+    double longitude;
+    String city;
+    String country;
+    String notes;
+
+
+
+}
